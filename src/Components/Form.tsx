945eb5cf66d63916489974cca855d79064d54a70
MendessWashington <mendesswashington@gmail.com>
2022-09-22 23:12:54 -0300

    Add function handleSubmit

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,20 +1,43 @@
+import { FormEvent, useState } from "react";
 import styles from "./Form.module.css";
 
 export const Form = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    console.log({
+      email,
+      password,
+    });
+  }
+
   return (
-    <form className={styles.content}>
+    <form onSubmit={handleSubmit} className={styles.content}>
       <header>
         <h1>Login</h1>
       </header>
 
       <div className={styles.contentInput}>
         <label htmlFor="email">E-mail</label>
-        <input id="email" type="email" name="email" />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
 
       <div className={styles.contentInput}>
         <label htmlFor="email">Password</label>
-        <input id="password" type="password" name="password" />
+        <input
+          id="password"
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
 
       <footer>
